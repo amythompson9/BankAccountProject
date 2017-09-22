2e38c07dbc2f24601967f6728473cf1533b63e4c
amythompson9 <amythompson9@gmail.com>
2017-09-21 20:44:05 -0400

    Finished Client Class

--- BankAccountProject/Client.cs
+++ BankAccountProject/Client.cs
@@ -17,8 +17,43 @@ namespace BankAccountProject
         private string clientFirstName;
         private string clientLastName;
         private string birthDate;
-        private float socialSecurityNumber;
+        private string socialSecurityNumber;
         private string clientAddress;
 
+        public string ClientFirstName
+        {
+            get { return this.clientFirstName; }
+        }
+        public string ClientLastName
+        {
+            get { return this.clientLastName; }
+        }
+        public string BirthDate
+        {
+            get { return this.birthDate; }
+        }
+        public string SocialSecurityNumber
+        {
+            get { return this.socialSecurityNumber; }
+        }
+        public string ClientAddress
+        {
+            get { return this.clientAddress; }
+        }
+        public Client(string clientFirstName, string clientLastName, string birthDate, string socialSecurityNumber, string clientAddress)
+        {
+            this.clientFirstName = clientFirstName;
+            this.clientLastName = clientLastName;
+            this.birthDate = birthDate;
+            this.socialSecurityNumber = socialSecurityNumber;
+            this.clientAddress = clientAddress;
+        }
+        public void ClientInformation()
+        {
+            Console.WriteLine("Name: " + "\n" + clientFirstName + " " + clientLastName + "\n");
+            Console.WriteLine("Birthdate: " +"\n" + birthDate + "\n");
+            Console.WriteLine("Social Security Number: " + "\n" + socialSecurityNumber + "\n");
+            Console.WriteLine("Current Address: " + "\n" +clientAddress + "\n");
+        }
     }
 }
--- BankAccountProject/Program.cs
+++ BankAccountProject/Program.cs
@@ -8,6 +8,16 @@ namespace BankAccountProject
 {
     class Program
     {
+        //1. View Client Information
+        //2. View Account Balance
+        //3. Deposit Funds
+        //4. Withdraw Funds
+        //5. Exit
+
+            static void MainMenu()
+        {
+            
+        }
         static void Main(string[] args)
         {
             //Program Class
@@ -25,6 +35,47 @@ namespace BankAccountProject
             //1 abstract method(at least)
             //1 virtual method(at least)
             //1 override method(at least)
+
+            Client client1 = new Client("John", "Smith", "March 14, 1985", "123 45 6789", "123 West Main Street \nWalla Walla, Washington 99362");
+            Console.WriteLine("Welcome to The Bank. You got money in it.");
+            Console.WriteLine();
+            Console.WriteLine("1. View Client Information \n2. View Account Balance \n3. Deposit Funds \n4. Withdraw Funds \n5. Exit");
+            int menuChoice = int.Parse(Console.ReadLine());
+            if (menuChoice == 1)
+            {
+                client1.ClientInformation();
+
+            }
+            else if (menuChoice == 2)
+            {
+                Console.WriteLine("1. Checking Account Balance \n2. Savings Account Balance");
+                int checkAccountBalance = int.Parse(Console.ReadLine());
+                if (checkAccountBalance == 1)
+                {
+
+                }
+                else if (checkAccountBalance == 2)
+                {
+                    
+                }
+            }
+            else if (menuChoice == 3)
+            {
+
+            }
+            else if (menuChoice == 4)
+            {
+
+            }
+            else if (menuChoice == 5)
+            {
+                Console.WriteLine("Thank you for using The Bank.");
+            }
+            else
+            {
+                Console.WriteLine("Please choose from the options listed below.");
+                MainMenu();
+            }
         }
     }
 }
